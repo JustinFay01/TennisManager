1a8c08b368aca56174264b0328e04d9e41316f41
Justin Fay <justinfay501@gmail.com>
2024-09-22 23:04:13 -0400

    Refactor import paths and add vite-tsconfig-paths plugin

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import '@/index.css';
 import { RouterProvider } from 'react-router-dom';
 
-import { router } from './router';
+import { router } from '@/app/router';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
