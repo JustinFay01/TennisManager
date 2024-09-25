26b342bfe14b039b5663ed03cc80c4f758063957
Justin Fay <justin.fay@hope.edu>
2024-09-24 23:33:47 -0400

    Storybook actually working!

--- src/app/routes/root.tsx
+++ src/app/routes/root.tsx
@@ -1,6 +1,7 @@
-import { MainLayout } from '@/components/layouts/main-layout';
 import { Outlet } from 'react-router-dom';
+
 import AppProvider from '@/app/provider';
+import { MainLayout } from '@/components/layouts/main-layout';
 
 export const AppRoot = () => {
   return (
