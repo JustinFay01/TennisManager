30798c20206b4d4c645988ced9cdb49bd885985a
Justin Fay <justinfay501@gmail.com>
2024-09-28 10:42:20 -0400

    lint fixes

--- src/lib/auth/protected-route.tsx
+++ src/lib/auth/protected-route.tsx
@@ -1,4 +1,4 @@
-import { useAuth0 } from '@auth0/auth0-react'
+//import { useAuth0 } from '@auth0/auth0-react'
 import { ErrorBoundary } from 'react-error-boundary'
 
 export const ProtectedRoute = ({ children }: React.PropsWithChildren) => {
