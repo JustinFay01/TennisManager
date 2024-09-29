7b38a549866011f2a9aa7babb10a3ad711e29eec
Justin Fay <justinfay501@gmail.com>
2024-09-28 22:52:32 -0400

    Stuck on reworking the hook

--- src/hooks/login-with-redirect.tsx
+++ src/hooks/login-with-redirect.tsx
@@ -1,23 +1,27 @@
 import { useLoginUser } from '@/api/login-user'
 import { useAuth0, RedirectLoginOptions } from '@auth0/auth0-react'
 
-export const useLoginWithRedirect = async (options: RedirectLoginOptions) => {
-  const { loginWithRedirect: auth0login, user } = useAuth0()
+export const useLoginWithRedirect = () => {
+  const { loginWithRedirect: auth0login, user, isAuthenticated } = useAuth0()
 
-  if (!user) {
-    throw new Error('You must be logged in to use this function')
+  // Conditionally create the login mutation only if the user is authenticated
+
+  const loginWithRedirect = async (options?: RedirectLoginOptions) => {
+    if (!isAuthenticated) {
+      // If the user is not logged in, redirect to login
+      return auth0login(options)
+    }
+
+    if (login) {
+      try {
+        // Trigger the login mutation if user exists
+        const data = await login.mutateAsync()
+        console.log(data)
+      } catch (error) {
+        console.error(error)
+      }
+    }
   }
 
-  const login = useLoginUser(user)
-
-  login
-    .mutateAsync()
-    .then((data) => {
-      console.log(data)
-    })
-    .catch((error) => {
-      console.error(error)
-    })
-
-  return auth0login(options)
+  return loginWithRedirect
 }
