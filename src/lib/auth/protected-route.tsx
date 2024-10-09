77958467b3bc62b22f0c1fedd00bb820ebc8a0b5
Justin Fay <justinfay501@gmail.com>
2024-10-08 22:34:04 -0400

    Refactor ProtectedRoute component to remove unnecessary console.log and isAuthenticated check

--- src/lib/auth/protected-route.tsx
+++ src/lib/auth/protected-route.tsx
@@ -1,15 +1,8 @@
 import { Loading } from '@/components/ui/feedback/loading/loading'
 import { WithChildren } from '@/types/props'
-import { useAuth0, withAuthenticationRequired } from '@auth0/auth0-react'
+import { withAuthenticationRequired } from '@auth0/auth0-react'
 
 export const ProtectedRoute = ({ children }: WithChildren) => {
-  const { isAuthenticated } = useAuth0()
-
-  console.log('isAuthenticated', isAuthenticated)
-  if (isAuthenticated) {
-    return <>{children}</>
-  }
-
   const Component = withAuthenticationRequired(() => <>{children}</>, {
     onRedirecting: () => <Loading />,
   })
