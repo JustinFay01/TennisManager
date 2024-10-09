465c8467b415c12fb0ad4e85853c0cdeba417d5a
Justin Fay <justinfay501@gmail.com>
2024-10-08 21:13:43 -0400

    Refactor main layout component

--- src/components/layouts/main-layout.tsx
+++ src/components/layouts/main-layout.tsx
@@ -3,9 +3,9 @@ import './main-layout.css'
 
 export const MainLayout = (props: { children: React.ReactNode }) => {
   return (
-    <div className="main">
+    <>
       <MainNav />
-      <main className="main-content">{props.children}</main>
-    </div>
+      {props.children}
+    </>
   )
 }
