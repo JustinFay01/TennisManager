77e10a2820d665f08360059f6ca52dadc9cea763
Justin Fay <justin.fay@hope.edu>
2024-10-09 21:23:51 -0400

    Refactor API files and remove unused code

--- src/api/coaches.ts
+++
@@ -1,8 +0,0 @@
-import { Coach } from '../types/props'
-
-import axios from '../lib/api-client'
-const entityUrl = '/coaches'
-
-export const getCoachesList = async () => {
-  return axios.get<Coach[]>(`${entityUrl}/all`)
-}
